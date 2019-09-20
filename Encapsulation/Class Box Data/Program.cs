3300133b0062afcd325e240ee568db8552cffc06
Kiril Simeonov <45626567+kirilsimeonov@users.noreply.github.com>
2019-09-20 15:11:45 +0300

    Class Box Data

---
+++ Encapsulation/Class Box Data/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ClassBoxData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double boxLength = double.Parse(Console.ReadLine());
+            double boxWidth = double.Parse(Console.ReadLine());
+            double boxHeight = double.Parse(Console.ReadLine());
+
+            try
+            {
+                var box = new Box(boxLength, boxWidth, boxHeight);
+
+                Console.WriteLine($"Surface Area - {box.CalculateSurfaceArea():F2}");
+                Console.WriteLine($"Lateral Surface Area - {box.CalculateLateralSurfaceArea():F2}");
+                Console.WriteLine($"Volume - {box.CalculateVolume():F2}");
+            }
+
+            catch (Exception ae)
+            {
+
+                Console.WriteLine(ae.Message);
+            }
+
+        }
+
+    }
+}
