ffecd8dd7f2007e897b4d3b2c2b3584ddaedf156
Kiril Simeonov <45626567+kirilsimeonov@users.noreply.github.com>
2019-09-23 19:01:30 +0300

    Football Team Generator

---
+++ Encapsulation/Football Team Generator/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FootballTeamGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var teams = new Dictionary<string, Team>();
+
+            while (true)
+            {
+                string[] input = Console.ReadLine().Split(';');
+                if (input[0] == "END")
+                {
+                    break;
+                }
+
+                string command = input[0];
+
+                try
+                {
+                    if (command == "Team")
+                    {
+                        string teamName = input[1];
+                        if (!teams.ContainsKey(teamName))
+                        {
+                            teams.Add(teamName, new Team(teamName));
+                        }
+
+                    }
+                    else if (command == "Add")
+                    {
+                        string teamName = input[1];
+                        string playerName = input[2];
+                        int endurance = int.Parse(input[3]);
+                        int sprint = int.Parse(input[4]);
+                        int dribble = int.Parse(input[5]);
+                        int passing = int.Parse(input[6]);
+                        int shooting = int.Parse(input[7]);
+
+                        if (!teams.ContainsKey(teamName))
+                        {
+                            Console.WriteLine($"Team {teamName} does not exist.");
+                        }
+                        else
+                        {
+                            teams[teamName].AddPlayer(new Player(playerName, endurance, sprint, dribble, passing, shooting));
+                        }
+                    }
+                    else if (command == "Remove")
+                    {
+                        string teamName = input[1];
+                        string playerName = input[2];
+
+                        if (teams[teamName].ContainsPlayer(playerName))
+                        {
+                            teams[teamName].RemovePlayer(playerName);
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Player {playerName} is not in {teamName} team.");
+                        }
+                    }
+                    else if (command == "Rating")
+                    {
+                        string teamName = input[1];
+
+                        if (!teams.ContainsKey(teamName))
+                        {
+                            Console.WriteLine($"Team {teamName} does not exist.");
+                        }
+                        else
+                        {
+                           Console.WriteLine($"{teamName} - {teams[teamName].Rating}"); 
+                        }
+                    }
+                }
+                catch (Exception ae)
+                {
+
+                    Console.WriteLine(ae.Message);
+                }
+               
+            }
+        }
+    }
+}
---
+++ Encapsulation/Football Team Generator/Team.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FootballTeamGenerator
+{
+    class Team
+    {
+        private string name;
+        private List<Player> players;
+
+        public Team(string name)
+        {
+            this.Name = name;
+            this.players = new List<Player>();
+        }
+        //public IReadOnlyCollection<Player> Players
+        //{
+        //    get
+        //    {
+        //        return players.AsReadOnly();
+        //    }
+        //}
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            private set
+            {
+                if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("A name should not be empty.");
+                }
+                else
+                {
+                    this.name = value;
+                }
+            }
+        }
+
+        public int Rating
+        {
+            get
+            {
+                if (players.Count > 0)
+                {
+                    return (int)Math.Round(players.Sum(n => n.AverageStats) / players.Count);
+                }
+                else
+                {
+                    return 0;
+                }
+                
+            }
+        }
+
+        public bool ContainsPlayer(string name)
+        {
+            return players.Exists(p => p.Name == name);
+        }
+
+        public void AddPlayer(Player player)
+        {
+            players.Add(player);
+        }
+        public void RemovePlayer(string name)
+        {
+            players.Remove(players.Where(p => p.Name == name).FirstOrDefault());
+        }
+    }
+}
