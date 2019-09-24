ec41848d2477bd956793d3950b73708d9757daa5
Kiril Simeonov <45626567+kirilsimeonov@users.noreply.github.com>
2019-09-24 21:08:55 +0300

    Person

---
+++ Inheritance/Person/Child.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace Person
+{
+    public class Child : Person
+    {
+
+
+
+        public override int Age
+        {
+            get
+            {
+                return base.Age;
+            }
+            set
+            {
+                //if (value > 15)
+                //{
+                //    throw new ArgumentException("Child's age must be less than 15!");
+                //}
+                //else
+                //{
+                base.Age = value;
+                //}
+            }
+        }
+        public Child(string name, int age)
+            : base(name, age)
+        {
+            this.Age = age;
+        }
+
+    }
+
+
+}
---
+++ Inheritance/Person/Person.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Text;
+
+namespace Person
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public string Name {
+            get {
+                return name;
+            }
+
+            set
+            {
+                if (value.Length<3)
+                {
+                    throw new ArgumentException("Name's length should not be less than 3 symbols!");
+                }
+                else
+                {
+                    name = value;
+                }
+            }
+        }
+        public virtual int Age {
+            get {
+                return age;
+            }
+            set
+            {
+                if (value<0)
+                {
+                    throw new ArgumentException("Age must be positive!");
+                }
+                else
+                {
+                    age = value;
+                }
+            }
+        }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"Name: {Name}, Age: {Age}");
+            return sb.ToString();
+        }
+    }
+}
