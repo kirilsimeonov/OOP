222d94a68f06f92e61fe8b3d984635f6a9cb2891
Kiril Simeonov <45626567+kirilsimeonov@users.noreply.github.com>
2019-09-24 21:10:20 +0300

    Zoo

---
+++ Inheritance/Zoo/Animal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Zoo
+{
+    public class Animal
+    {
+        public string Name { get; set; }
+
+        public Animal(string name)
+        {
+            Name = name;
+        }
+    }
+}
