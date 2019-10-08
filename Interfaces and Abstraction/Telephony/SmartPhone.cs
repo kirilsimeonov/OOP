dfb153461aa4945952a7a48e270244820e0431a8
Kiril Simeonov <45626567+kirilsimeonov@users.noreply.github.com>
2019-10-08 13:10:39 +0300

    Telephony

---
+++ Interfaces and Abstraction/Telephony/SmartPhone.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Telephony
+{
+    class SmartPhone : ICallMaker, IBrowser
+    {
+        public void Browse(string website)
+        {
+            var regex = new Regex(@"^\D*$");
+
+            if (regex.IsMatch(website))
+            {
+                Console.WriteLine($"Browsing: {website}!");
+            }
+            else
+            {
+                Console.WriteLine("Invalid URL!");
+            }
+        }
+
+        public void Call(string number)
+        {
+            var regex = new Regex(@"^\d+$");
+
+            if (regex.IsMatch(number))
+            {
+                Console.WriteLine($"Calling... {number}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid number!");
+            }
+        }
+    }
+}
---
+++ Interfaces and Abstraction/Telephony/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Telephony
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] numbers = Console.ReadLine().Split(' ',StringSplitOptions.RemoveEmptyEntries);
+            string[] websites = Console.ReadLine().Split();
+
+            var phone = new SmartPhone();
+
+            foreach (string number in numbers)
+            {
+                phone.Call(number);
+            }
+
+            foreach (string website in websites)
+            {
+                phone.Browse(website);
+            }
+        }
+    }
+}
